5bf1cffbd258ca211ebe0901cec296ebaf57aa5b
Kristian Zanev <kristianzanev@hotmail.com>
2021-08-15 22:50:03 +0300

    fix: cross-site tests

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
+
+fetch('http://localhost:3000', {
+  method: 'GET',
+  credentials: 'include',
+}).then(async (res) => {
+  const data = await res.json();
+  console.warn(data);
+});
